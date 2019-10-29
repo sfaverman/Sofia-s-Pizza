7dec1d19c3de8fbe6c432de97e8356aa560c8be7
sfaverman <sofiasd@yahoo.com>
2019-10-29 14:33:43 -0700

    Added live search to order.html

--- scripts/search.js
+++ scripts/search.js
@@ -2,6 +2,8 @@
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 */
 
+/*alert ("You are on a search page");*/
+
 $('#search').keyup(function(){
 
 /* keyup = when user typing and releasing key */
@@ -9,7 +11,7 @@ $('#search').keyup(function(){
     var myExp = new RegExp(searchField, 'i');
     /* i stands for case insensitive */
 
-    $.getJSON('menu.json', function(data){
+    $.getJSON('../menu.json', function(data){
         console.log(data);
         var output = '<ul class="searchResults">';
 
@@ -20,9 +22,10 @@ $('#search').keyup(function(){
 
                 output += '<li>';
                 output += '<h2>'+val.product + '</h2>';
-                output += '<img src ="search-image/'+val.image+'.jpg" alt="'+val.name+'" />';
+                output += '<img src ="../images/products/'+val.image+'.jpg" alt="'+val.name+'" />';
                 output += '<p>'+ val.price + '</p>';
                 output += '<p><em>'+ val.description + '</em></p>';
+				output += '<a href="#" class="btn button ">Add to Cart !</a>';
                 output += '</li>';
             }
         });
